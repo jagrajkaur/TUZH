496e602499ca661f41b9869c2c99bdb0ae33506e
Jagraj Kaur <37617281+jagrajkaur@users.noreply.github.com>
2024-02-16 15:06:34 -0500

    - Initial Project setup:     - Configured Vite (modern build tool used for faster and efficient development experience which can be used with various front-end frameworks like React, Vue.js etc) with Tailwind CSS    - Installed required react libraries like react-router-dom react-icons react-spinners react-toastify swiper

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-[20px] text-blue-600'>TUZH - The Ultimate Zen Hub</h1>
+    </>
+  )
+}
+
+export default App
