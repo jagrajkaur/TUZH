0ee4153b4f85ae0e345f9599ba36af7749516f23
Jagraj Kaur <37617281+jagrajkaur@users.noreply.github.com>
2024-02-24 17:10:11 -0500

    Reverted frontend changes
    - Initial Project setup:
        - Configured Vite (modern build tool used for faster and efficient development experience which can be used with various front-end frameworks like React, Vue.js etc) with Tailwind CSS
       - Installed required react libraries like react-router-dom react-icons react-spinners react-toastify swiper
      - Implemented Header component and integrated application logo

---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,19 @@
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Contact from "../pages/Contact";
+
+import { Routes, Route } from "react-router-dom";
+
+const Routers = () => {
+    return <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/register" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Signup/>} />
+        <Route path="/contact" element={<Contact/>} />
+    </Routes>
+};
+
+export default Routers;
