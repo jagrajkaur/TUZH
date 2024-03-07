6261fd98bdda22c47dfa547eefc69f2e03a4f52e
Jagraj Kaur <37617281+jagrajkaur@users.noreply.github.com>
2024-03-07 02:28:17 -0500

    Implemented footer UI

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,25 +1,149 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from "../../assets/images/logo.png";
+import {RiLinkedinFill} from 'react-icons/ri';
+import { SiJirasoftware } from "react-icons/si";
+import {AiFillYoutube, AiFillGithub, AiOutlineInstagram} from 'react-icons/ai';
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
+const socialLinks = [
+  {
+    path: "",
+    icon: <AiFillYoutube className='group-hover:text-white w-4 h-5' />,
+  },
+  {
+    path: "",
+    icon: <AiFillGithub className='group-hover:text-white w-4 h-5' />,
+  },
+  {
+    path: "",
+    icon: <AiOutlineInstagram className='group-hover:text-white w-4 h-5' />,
+  },
+  {
+    path: "",
+    icon: <SiJirasoftware className='group-hover:text-white w-4 h-5' />,
+  },
+];
+
+const quickLinks01 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path:"/mytasks",
+    display:"My Task",
+  },
+  {
+      path:"/bookappointment",
+      display:"Book Appointment",
+  },
+  {
+      path:"/contact",
+      display:"Contact",
+  },
+];
+
+const quickLinks02 = [
+  {
+    path: "/find-a-doctor",
+    display: "Find a Doctor",
+  },
+  {
+    path:"/",
+    display:"Request an Appointment",
+  },
+  {
+      path:"/",
+      display:"Find a Location",
+  },
+  {
+      path:"/",
+      display:"Get a Opinion",
+  },
+];
+
+const quickLinks03 = [
+  {
+    path: "/",
+    display: "Donate",
+  },
+  {
+    path:"/contact",
+    display:"Contact Us",
+  },
+];
+
 const Footer = () => {
+  const year = new Date().getFullYear();
+
   return (
-    <footer className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="flex justify-center md:justify-start items-center mb-4 md:mb-0">
-          <p className="text-sm mr-4">Follow Us:&nbsp;&nbsp;<span className="text-yellow-500">@TUZH_CANADA</span></p>
-          <div className="flex space-x-2">
-            <a href="#" className="text-white hover:text-blue-400">
-              <FaFacebook />
-            </a>
-            <a href="#" className="text-white hover:text-blue-400">
-              <FaTwitter />
-            </a>
-            <a href="#" className="text-white hover:text-blue-400">
-              <FaInstagram />
-            </a>
+    <footer className='pb-16 pt-10'>
+      <div className='container'>
+        <div className='flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
+          <div>
+            <img src={logo} alt="" className='h-16' />
+            <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>
+              Copyright &copy; {year} developed by Alpha Stack all right reserved.
+            </p>
+
+            <div className='flex items-center gap-3 mt-4'>
+              {socialLinks.map((link, index) => (
+                <Link
+                  to={link.path}
+                  key={index}
+                  className='w-9 h-9 border border-solid border-[#181A1E] rounded-full flex items-center 
+                  justify-center group hover:bg-primarycolor hover:border-none'
+                >
+                  {link.icon}
+                </Link>
+              ))}
+            </div>
           </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+              Quick Links
+            </h2>
+
+            <ul>
+              {quickLinks01.map((item,index) => (
+                <li key={index} className='mb-4'>
+                  <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>{item.display}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+              I want to:
+            </h2>
+
+            <ul>
+              {quickLinks02.map((item,index) => (
+                <li key={index} className='mb-4'>
+                  <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>{item.display}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>
+              Support
+            </h2>
+
+            <ul>
+              {quickLinks03.map((item,index) => (
+                <li key={index} className='mb-4'>
+                  <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>{item.display}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
         </div>
-        <p className="text-sm">&copy; TUZH. 2024. All rights reserved.</p>
       </div>
     </footer>
   );
